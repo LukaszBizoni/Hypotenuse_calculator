5782863ae354652c6c9e129469c60d4b83ffcde3
rbe1ro <lucasribeirobizoni@gmail.com>
2024-11-23 23:00:56 -0300

    concertando bug do fim do programa

--- test.c
+++ test.c
@@ -3,22 +3,19 @@
 #include <locale.h>
 
 int main(){
-	setlocale(LC_ALL, "Portuguese");
-	int cateto1, cateto2;
-	float hipotenusa;
-	
-	printf("insira o primeiro cateto: ");
-	scanf("%d", &cateto1);
-	
-	printf("insira o segundo cateto: ");
-	scanf("%d", &cateto2);
-	
-	hipotenusa = pow(cateto2,2) + pow(cateto1,2); 
-	hipotenusa = sqrt(hipotenusa);
-	
-	printf("a sua hipotenusa é de: \n");
-	printf("%f", hipotenusa);
+    setlocale(LC_ALL, "Portuguese");
+    int cateto1, cateto2;
+    float hipotenusa;
 
-	return 0;
-	
-}
+    printf("insira o primeiro cateto: ");
+    scanf("%d", &cateto1);
+
+    printf("insira o segundo cateto: ");
+    scanf("%d", &cateto2);
+
+    hipotenusa = pow(cateto2,4) + pow(cateto1,2);
+    hipotenusa = sqrt(hipotenusa);
+
+    printf("a sua hipotenusa eh de:\n %f",hipotenusa);
+
+    return 0;
